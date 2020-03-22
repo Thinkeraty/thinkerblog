c07c39b35376debe1f069c2d343fd682c5d22a48
Thinkeraty <contact.thinkeraty@gmail.com>
2020-03-22 14:15:27 +0530

    modified db

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express')
-const mongoose = require('mongoose')
 const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
 const app = express()
-
-mongoose.connect('mongodb://localhost/blog', {
-  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
-})
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
@@ -17,6 +16,14 @@ app.get('/', async (req, res) => {
   const articles = await Article.find().sort({ createdAt: 'desc' })
   res.render('articles/index', { articles: articles })
 })
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/articles', articleRouter)
 
