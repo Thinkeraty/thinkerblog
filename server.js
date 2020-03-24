0c2204b1cf81eb6dcddb7bd24a2cfe79579f0c84
Thinkeraty <contact.thinkeraty@gmail.com>
2020-03-24 21:28:15 +0530

    hi

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV !== 'production') {
+/*if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
@@ -11,16 +11,16 @@ const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
 const app = express()
 
-//app.set('view engine', 'ejs')
+app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
 
 app.get('/', async (req, res) => {
   const articles = await Article.find().sort({ createdAt: 'desc' })
-  res.render('index.html'/*, { articles: articles }*/)
+  res.render('articles/index', { articles: articles })
 })
 
-/*const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { 
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -29,8 +29,8 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
-app.use('/articles', articleRouter)*/
+app.use('/articles', articleRouter)
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,  () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT,  () => console.log(`Server running on port ${PORT}`));*/
